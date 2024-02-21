2b4c95e9a0bbaa8d0f0a1c39ba5962f4763c9a6e
Memo Ugurbil <memo@nillion.com>
2024-02-21 16:42:24 -0600

    debug plain and add float giving precision 97.3%

--- src/float_lr.rs
+++ src/float_lr.rs
@@ -1,49 +1,5 @@
 use std::fs::File;
 
-fn to_signed(x: u64) -> i64 {
-    if x > (1u64 << 63) {
-        (x as i128 - (1i128 << 64)) as i64
-    } else {
-        x as i64
-    }
-}
-
-fn from_signed(x: i64) -> u64 {
-    (x as i128).rem_euclid(1i128 << 64) as u64 
-}
-
-fn quantize(x: f64, precision: u8) -> u64 {
-    from_signed((x * ((1u128 << precision) as f64)) as i64)
-}
-
-fn unquantize(x: u64, precision: u8) -> f64 {
-    to_signed(x) as f64 / ((1u128 << precision) as f64)
-}
-
-fn add(a: u64, b: u64) -> u64 {
-    (a as u128 + b as u128).rem_euclid(1u128 << 64) as u64 
-}
-
-fn mul(a: u64, b: u64) -> u64 {
-    (a as u128 * b as u128).rem_euclid(1u128 << 64) as u64
-}
-
-fn truncate(x: u64, precision: u8) -> u64 {
-    from_signed(to_signed(x) / (1i64 << precision))
-}
-
-fn sigmoid(x: u64, input_precision: u8, output_precision: u8) -> u64 {
-    let x = if x > (1u64 << 63) {
-        (x as i128 - 1i128 << 64) as f64
-    } else {
-        x as f64
-    };
-    let shift = (1u128 << input_precision) as f64;
-    let exp = (x / shift).exp();
-    (exp * ((1u128 << output_precision) as f64)) as u64
-    // ((1.0 / (1.0 + exp)) * (1 << output_precision) as f64) as u64
-}
-
 fn argmax<T: PartialOrd>(slice: &[T]) -> Option<usize> {
     slice
         .iter()
@@ -65,27 +21,6 @@ fn load_weights_and_biases() -> (Vec<Vec<f64>>, Vec<f64>) {
     }
 
     (weights, biases)
-}
-
-fn quantize_weights_and_biases(
-    weights: &[Vec<f64>],
-    biases: &[f64],
-    precision: u8,
-) -> (Vec<Vec<u64>>, Vec<u64>) {
-    let weights_int = weights
-        .iter()
-        .map(|row| {
-            row.iter()
-                .map(|&w| quantize(w.into(), precision))
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-    let bias_int = biases
-        .iter()
-        .map(|&w| quantize(w.into(), precision))
-        .collect::<Vec<_>>();
-
-    (weights_int, bias_int)
 }
 
 fn prepare_iris_dataset() -> Vec<(Vec<f64>, usize)> {
@@ -125,9 +60,7 @@ fn means_and_stds(dataset: &[(Vec<f64>, usize)], num_features: usize) -> (Vec<f6
 }
 
 fn main() {
-    let precision = 16;
     let (weights, biases) = load_weights_and_biases();
-    let (weights_int, bias_int) = quantize_weights_and_biases(&weights, &biases, precision);
 
     let iris_dataset = prepare_iris_dataset();
     let num_features = iris_dataset[0].0.len();
@@ -136,8 +69,8 @@ fn main() {
     let mut total = 0;
     for (num, (sample, target)) in iris_dataset.iter().enumerate() {
         let mut probabilities = vec![];
-        let mut sum = 0u64;
-        for (model, bias) in weights_int.iter().zip(bias_int.iter()) {
+        let mut sum_p = 0f64;
+        for (model, bias) in weights.iter().zip(biases.iter()) {
             let mut prediction = *bias;
             for ((&s, w), (mean, std)) in sample
                 .iter()
@@ -145,19 +78,17 @@ fn main() {
                 .zip(means.iter().zip(stds.iter()))
             {
                 let n = (s - mean) / std;
-                let quantized = quantize(n, precision);
-                let cur = truncate(mul(*w, quantized), precision);
-                prediction = add(prediction, cur);
+                prediction += w * n;
             }
-            let activation = sigmoid(prediction, precision, precision);
+            let activation = prediction.exp();
             probabilities.push(activation);
-            sum = add(sum, activation);
+            sum_p += activation;
+        }
+        let mut norm = Vec::new();
+        for p in &probabilities {
+            norm.push(p / sum_p);
         }
         let class = argmax(&probabilities).unwrap();
-        let mut norm = vec![];
-        for p in probabilities {
-            norm.push(p as f32 / sum as f32);
-        }
         println!("[{}] predicted {:?}, target {:?} probabilities {:?}", num, class, target, norm);
         if class == *target {
             total += 1;
